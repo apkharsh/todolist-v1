1821ce05b460053f986addc629cad9878ddd3478
apkharsh <apkharsh@gmail.com>
2022-07-08 00:36:12 +0530

    started working with ejs

--- app.js
+++ app.js
@@ -1,10 +1,19 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const app = express();
+app.use('view engine', 'ejs');
+
 app.get('/', function(req, res) {
-    res.send('Hello World!');
+    // res.send('Hello World!');
+    var today = new Date();
+    if(today.getDay() === 4){
+        res.send('Today is Thursday!');
+    }
+    else{
+        res.send('Today is not Thursday!');
+    }
 });
 
 app.listen(3000, function() {
     console.log('Example app listening on port 3000!');
-}
+});
