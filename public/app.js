662f7f9329190d2af1762a0577dd35e72ff6e228
apkharsh <apkharsh@gmail.com>
2022-07-11 12:27:26 +0530

    MINOR CHANGES

--- public/app.js
+++ public/app.js
@@ -58,5 +58,5 @@ app.post("/", function (req, res) {
 });
 
 app.listen(3000, function () {
-    console.log("Example app listening on port 3000!");
+    console.log("Example app listening on port 6000!");
 });
